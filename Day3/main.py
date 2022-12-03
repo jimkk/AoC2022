b4cb97370590552a1b1f6ba4e57bbac9b8d84800
Jim <jim-m-kemper@hotmail.com>
2022-12-03 00:23:20 -0700

    Day 3 beautiful oneliner

--- Day3/main.py
+++ Day3/main.py
@@ -9,6 +9,7 @@ priority = sum([y - (96 if y >= 97 else 38) for y in [ord(x) for x in sacks]])
 print(priority)
 
 #Part 2
+#print(sum([y - (96 if y >= 97 else 38) for y in [ord(x) for x in [list(set(x).intersection(y).intersection(z))[0] for x,y,z in [[s for s in s_input.split('\n')][x*3:x*3+3] for x in range(int(len([s for s in s_input.split('\n')])/3))]]]]))
 sets = [s for s in s_input.split('\n')]
 subsets = [sets[x*3:x*3+3] for x in range(int(len(sets)/3))]
 badges = [list(set(x).intersection(y).intersection(z))[0] for x,y,z in subsets]
