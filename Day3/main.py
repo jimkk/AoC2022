1a8191e72c155c907082c569ec81a0a460b46114
Jim <jim-m-kemper@hotmail.com>
2022-12-03 22:44:06 -0700

    day 4

--- Day3/main.py
+++ Day3/main.py
@@ -12,6 +12,6 @@ print(priority)
 #print(sum([y - (96 if y >= 97 else 38) for y in [ord(x) for x in [list(set(x).intersection(y).intersection(z))[0] for x,y,z in [[s for s in s_input.split('\n')][x*3:x*3+3] for x in range(int(len([s for s in s_input.split('\n')])/3))]]]]))
 sets = [s for s in s_input.split('\n')]
 subsets = [sets[x*3:x*3+3] for x in range(int(len(sets)/3))]
-badges = [list(set(x).intersection(y).intersection(z))[0] for x,y,z in subsets]
+badges = [list(set(x) & set(y) & set(z))[0] for x,y,z in subsets]
 priority = sum([y - (96 if y >= 97 else 38) for y in [ord(x) for x in badges]])
 print(priority)
